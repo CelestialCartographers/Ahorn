8d97b3bacefed5ba450395b111537b2395705671
Maik Macho <nuda1998@gmail.com>
2018-06-19 20:25:13 +0200

    Added Dark Chaser.

---
+++ src/entities/darkchaser.jl
@@ -0,0 +1,60 @@
+module DarkChaser
+
+placements = Dict{String, Main.EntityPlacement}(
+    "Badeline Chaser" => Main.EntityPlacement(
+        Main.Maple.DarkChaser
+    ),
+
+    "Badeline Chaser Barrier" => Main.EntityPlacement(
+        Main.Maple.DarkChaserEnd,
+        "rectangle"
+    ),
+)
+
+function minimumSize(entity::Main.Maple.Entity)
+    if entity.name == "darkChaserEnd"
+        return true, 8, 8
+    end
+end
+
+function resizable(entity::Main.Maple.Entity)
+    if entity.name == "darkChaserEnd"
+        return true, true, true
+    end
+end
+
+function selection(entity::Main.Maple.Entity)
+    if entity.name == "darkChaser"
+        x, y = Main.entityTranslation(entity)
+
+        return true, Main.Rectangle(x - 2, y - 16, 12, 16)
+    end
+
+    if entity.name == "darkChaserEnd"
+        x, y = Main.entityTranslation(entity)
+
+        width = Int(get(entity.data, "width", 8))
+        height = Int(get(entity.data, "height", 8))
+
+        return true, Main.Rectangle(x, y, width, height)
+    end
+end
+
+function render(ctx::Main.Cairo.CairoContext, entity::Main.Maple.Entity)
+    if entity.name == "darkChaser"
+        Main.drawSprite(ctx, "characters/badeline/sleep00.png", 4, -16)
+
+        return true
+    end
+
+    if entity.name == "darkChaserEnd"
+        width = Int(get(entity.data, "width", 32))
+        height = Int(get(entity.data, "height", 32))
+
+        Main.drawRectangle(ctx, 0, 0, width, height, (0.4, 0.0, 0.4, 0.4), (0.4, 0.0, 0.4, 1.0))
+
+        return true
+    end
+end
+
+end
