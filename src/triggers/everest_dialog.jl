b3531556a93f728fc72c58a00bd3b7a539b825c4
Cruor <cruoratmc@gmail.com>
2020-02-15 16:52:06 +0100

    Added new entities and attributes.
    Added onlyOnce, dialogId, and deathCount to Everest Dialog Trigger.
    Added xOnly and yOnly to Everest Smooth Camera Trigger.
    Added chapter 9 NPCs.
    Added chapter 9 events.
    Added summit and onlyY attributes to Lookouts.
    Added below attribute to Space Jam.
    Added playerCanUse attribute to wall springs.
    Added onlyOnce and onlyIfPlayerLeft attributes to Bird NPC.
    Added deleteFlag attribute to Camera Target Trigger.
    Fixed weird definition in Everest Dialog Trigger.
    Updated info in existing death count tooltips.

--- src/triggers/everest_dialog.jl
+++ src/triggers/everest_dialog.jl
@@ -5,11 +5,7 @@ using ..Ahorn, Maple
 const placements = Ahorn.PlacementDict(
     "Dialog Cutscene (Everest)" => Ahorn.EntityPlacement(
         Maple.EverestDialogTrigger,
-        "rectangle",
-        Dict{String, Any}(
-            "dialogId" => "",
-            "onlyOnce" => true
-        )
+        "rectangle"
     )
 )
 
